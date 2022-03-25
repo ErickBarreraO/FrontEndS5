d776f7d66e7212ce858fbe3b541961c3855aa545
ErickBarreraO <99240888+ErickBarreraO@users.noreply.github.com>
2022-03-25 15:37:59 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+    data() {
+        return{
+            nombre: 'Erick',
+            apellido: "Barrera",
+            email: 'erick@hotmail.com',
+            genero: 'male',
+            foto: 'https://randomuser.me/api/portraits/men/10.jpg'
+        }
+    },
+    methods: {
+        async cambiarusuario() {
+            const res = await fetch('https://randomuser.me/api');
+            const {results} = await res.json()
+            //console.log(results)
+            
+            this.nombre = results[0].name.first
+            this.apellido= results[0].name.last
+            this.email= results[0].email
+            this.genero= results[0].gender
+            this.foto= results[0].picture.large
+        }
+    }
+})
+
+app.mount('#app')
+
+
